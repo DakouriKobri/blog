95a77f4fe3713c751a9106ec3f61b7b27eae2265
Dakouri M. Kobri <maurille.kobri@gmail.com>
2022-09-13 10:23:08 +0200

    initial commit

---
+++ comments/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id]) || [];
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  /* ======================================= THIS CAUSES ERROR WHEN UNCOMMENTED =======================================
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  ====================================================================================================================
+ */
+
+  res.status(201).send(comments);
+});
+
+PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
